dff4b51a6ee7578e165930afdafe959205b9b37b
Ilya <121868806+Lio-Kay@users.noreply.github.com>
2023-05-10 02:41:01 +0700

    created conftest replaced files with fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,268 @@
+import pytest
+
+
+@pytest.fixture
+def data_to_test_get_data_from_file__to_test_visualize_operations():
+    return [{
+        "id": 114832369,
+        "state": "EXECUTED",
+        "date": "2019-12-07T06:17:14.634890",
+        "operationAmount": {
+            "amount": "48150.39",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Visa Classic 2842878893689012",
+        "to": "Счет 35158586384610753655"
+    }]
+
+
+@pytest.fixture
+def data_to_test_validate_data_in():
+    return [{
+        "id": 587085106,
+        "state": "EXECUTED",
+        "date": "2018-03-23T10:45:06.972075",
+        "operationAmount": {
+            "amount": "48223.05",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 41421565395219882431"
+    },
+        {
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        }]
+
+
+@pytest.fixture
+def data_to_test_validate_data_out():
+    return [{"id": 587085106,
+             "state": "EXECUTED",
+             "date": "2018-03-23T10:45:06.972075",
+             "operationAmount": {
+                 "amount": "48223.05",
+                 "currency": {
+                     "name": "руб.",
+                     "code": "RUB"
+                 }
+             },
+             "description": "Открытие вклада",
+             "to": "Счет 41421565395219882431"
+             },
+            {
+                "id": 873106923,
+                "state": "EXECUTED",
+                "date": "2019-03-23T01:09:46.296404",
+                "operationAmount": {
+                    "amount": "43318.34",
+                    "currency": {
+                        "name": "руб.",
+                        "code": "RUB"
+                    }
+                },
+                "description": "Перевод со счета на счет",
+                "from": "Счет 44812258784861134719",
+                "to": "Счет 74489636417521191160"
+            }]
+
+
+@pytest.fixture
+def data_to_test_get_executed_operations_in():
+    return [{
+        "id": 863064926,
+        "state": "EXECUTED",
+        "date": "2019-12-08T22:46:21.935582",
+        "operationAmount": {
+            "amount": "41096.24",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 90424923579946435907"
+    },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }]
+
+
+@pytest.fixture
+def data_to_test_get_executed_operations_out():
+    return [{
+        "id": 863064926,
+        "state": "EXECUTED",
+        "date": "2019-12-08T22:46:21.935582",
+        "operationAmount": {
+            "amount": "41096.24",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 90424923579946435907"
+    }]
+
+
+@pytest.fixture
+def data_to_test_sort_operations_in():
+    return [{
+        "id": 147815167,
+        "state": "EXECUTED",
+        "date": "2018-01-26T15:40:13.413061",
+        "operationAmount": {
+            "amount": "50870.71",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод с карты на счет",
+        "from": "Maestro 4598300720424501",
+        "to": "Счет 43597928997568165086"
+    },
+        {
+            "id": 518707726,
+            "state": "EXECUTED",
+            "date": "2018-11-29T07:18:23.941293",
+            "operationAmount": {
+                "amount": "3348.98",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "MasterCard 3152479541115065",
+            "to": "Visa Gold 9447344650495960"
+        },
+        {
+            "id": 649467725,
+            "state": "EXECUTED",
+            "date": "2018-04-14T19:35:28.978265",
+            "operationAmount": {
+                "amount": "96995.73",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 27248529432547658655",
+            "to": "Счет 97584898735659638967"
+        },
+        {
+            "id": 782295999,
+            "state": "EXECUTED",
+            "date": "2019-09-11T17:30:34.445824",
+            "operationAmount": {
+                "amount": "54280.01",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 24763316288121894080",
+            "to": "Счет 96291777776753236930"
+        }]
+
+
+@pytest.fixture
+def data_to_test_sort_operations_out():
+    return [
+        {
+            "id": 782295999,
+            "state": "EXECUTED",
+            "date": "2019-09-11T17:30:34.445824",
+            "operationAmount": {
+                "amount": "54280.01",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 24763316288121894080",
+            "to": "Счет 96291777776753236930"
+        },
+        {
+            "id": 518707726,
+            "state": "EXECUTED",
+            "date": "2018-11-29T07:18:23.941293",
+            "operationAmount": {
+                "amount": "3348.98",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "MasterCard 3152479541115065",
+            "to": "Visa Gold 9447344650495960"
+        },
+        {
+            "id": 649467725,
+            "state": "EXECUTED",
+            "date": "2018-04-14T19:35:28.978265",
+            "operationAmount": {
+                "amount": "96995.73",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 27248529432547658655",
+            "to": "Счет 97584898735659638967"
+        },
+        {
+            "id": 147815167,
+            "state": "EXECUTED",
+            "date": "2018-01-26T15:40:13.413061",
+            "operationAmount": {
+                "amount": "50870.71",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Maestro 4598300720424501",
+            "to": "Счет 43597928997568165086"
+        }
+    ]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,41 +1,26 @@
 from utils import main
 
 
-"""
-Я наверное собрал все антипаттерны тестирования в одном файле
-"""
-
-def test_get_data_from_file():
-    assert main.get_data_from_file('data_to_test_get_data_from_file_1.json') == [{
-    "test id": 0,
-    "test state": "FAILED",
-    "test date": "32.12.1999",
-    "test operationAmount": {
-      "amount": "-99.43",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    }}]
+def test_get_data_from_file(data_to_test_get_data_from_file__to_test_visualize_operations):
+    assert main.get_data_from_file('data_to_test_get_data_from_file_1.json') ==\
+           data_to_test_get_data_from_file__to_test_visualize_operations
     assert main.get_data_from_file('data_to_test_get_data_from_file_2.json') == {}
 
 
-def test_validate_data():
-    assert main.validate_data(main.get_data_from_file('data_to_test_validate_data_1.json'))\
-           == main.get_data_from_file('data_to_test_validate_data_2.json')
+def test_validate_data(data_to_test_validate_data_in, data_to_test_validate_data_out):
+    assert main.validate_data(data_to_test_validate_data_in) == data_to_test_validate_data_out
 
 
-def test_get_executed_operations():
-    assert main.get_executed_operations(main.get_data_from_file('data_to_test_get_executed_operations_1.json'))\
-           == main.get_data_from_file('data_to_test_get_executed_operations_2.json')
+def test_get_executed_operations(data_to_test_get_executed_operations_in, data_to_test_get_executed_operations_out):
+    assert main.get_executed_operations(data_to_test_get_executed_operations_in) \
+           == data_to_test_get_executed_operations_out
 
 
-def test_sort_operations():
-    assert main.sort_operations(main.get_data_from_file('data_to_test_sort_operations_1.json'))\
-           == (main.get_data_from_file('data_to_test_sort_operations_2.json'))
+def test_sort_operations(data_to_test_sort_operations_in, data_to_test_sort_operations_out):
+    assert main.sort_operations(data_to_test_sort_operations_in) == data_to_test_sort_operations_out
 
 
-def test_visualize_operations(capfd):
-    main.visualize_operations(main.get_data_from_file('data_to_test_visualize_operations.json'))
+def test_visualize_operations(data_to_test_get_data_from_file__to_test_visualize_operations, capfd):
+    main.visualize_operations(data_to_test_get_data_from_file__to_test_visualize_operations)
     out, err = capfd.readouterr()
     assert out == '07.12.2019 Перевод организации\nVisa Classic 2842 87** **** 9012 -> Счет **3655\n48150.39 USD\n\n'
